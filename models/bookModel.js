b84a4ccf266c68a2dcaaf209fc4e9507d162baf4
Diego Medel <di@Diegos-MacBook-Pro-2.local>
2025-04-26 18:46:54 -0600

    feat: actualizar lista de libros con cómics de Marvel relacionados a Scarlet Witch, X-Men y Wiccan

--- models/bookModel.js
+++ models/bookModel.js
@@ -1,8 +1,14 @@
-// models/bookModel.js
 const books = [
-  { title: "El Principito", author: "Antoine de Saint-Exupéry" },
-  { title: "Cien Años de Soledad", author: "Gabriel García Márquez" },
-  { title: "1984", author: "George Orwell" }
+  { title: "Avengers: The Children's Crusade", author: "Allan Heinberg" },
+  { title: "House of M", author: "Brian Michael Bendis" },
+  { title: "Scarlet Witch Vol. 1: Witches' Road", author: "James Robinson" },
+  { title: "Scarlet Witch Vol. 2: World of Witchcraft", author: "James Robinson" },
+  { title: "Young Avengers Vol. 1: Style > Substance", author: "Kieron Gillen" },
+  { title: "Young Avengers Vol. 2: Alternative Culture", author: "Kieron Gillen" },
+  { title: "Vision and the Scarlet Witch", author: "Steve Englehart" },
+  { title: "X-Men: The Dark Phoenix Saga", author: "Chris Claremont" },
+  { title: "X-Men: Days of Future Past", author: "Chris Claremont" },
+  { title: "Uncanny X-Men: The Birth of Generation Hope", author: "Matt Fraction" }
 ];
 
 module.exports = books;
